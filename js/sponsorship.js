7e96a8b1da71c6210d64e690dccd5a28596630b9
Mike Elsmore <mike@elsmore.me>
2015-05-13 11:11:16 +0100

    Clean up master

--- js/sponsorship.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  var onHoverIn = function(){
-    $(this).children('sub').removeClass('hidden');
-  }
-  var onHoverOut = function(){
-    $(this).children('sub').addClass('hidden');
-  }
-  $('#sponsorship-tiers th').hover(onHoverIn,onHoverOut);
-});
